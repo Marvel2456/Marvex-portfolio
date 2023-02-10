760b9461e148f9a346682d23dbc669dbebe9c8d1
Marvel2456 <marvellousidemudia@gmail.com>
2023-02-10 16:48:19 +0100

    First portfolio commit

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full text-white py-16'>
+        <div className=''>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='w-full text-white py-16'>
+      
+    </div>
+  )
+}
+
+export default Experience
