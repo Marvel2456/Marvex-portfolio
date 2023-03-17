a79a557dd2f3dc81754ec8da181adafba40973bd
Marvel2456 <marvellousidemudia@gmail.com>
2023-03-17 15:40:19 +0100

    Added link to live app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -67,11 +67,13 @@ const Projects = () => {
             <div className='opacity-0 group-hover:opacity-100'>
 
               <span className='text-2xl font-bold tracking-wider'>
-                Django Application
+                <p>Django Application</p>
+                <p>username: admin</p>
+                <p>password:admin123</p>
               </span>
 
               <div className='pt-8 text-center'>
-                <a href="/">
+                <a href="https://quicksales-2.up.railway.app/">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href="https://github.com/Marvel2456/Quicksales-2.0">
